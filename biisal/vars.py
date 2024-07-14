df6fcab6ca4553e68347ef89d4f4d50236e5ca82
Ansh vachhani <92299607+Anshvachhani99@users.noreply.github.com>
2024-07-14 21:59:04 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-bot_name = "Bɪɪsᴀʟ Fɪʟᴇ2Lɪɴᴋ Bᴏᴛ"
-bisal_channel = "https://telegram.me/bisal_files"
-bisal_grp = "https://t.me/+PA8OPL2Zglk3MDM1"
+bot_name = "ᴀɴs Fɪʟᴇ2Lɪɴᴋ Bᴏᴛ"
+bisal_channel = "https://t.me/+aE7D-Fd-gBkyMjk1"
+bisal_grp = "https://t.me/+aE7D-Fd-gBkyMjk1"
 
 class Var(object):
     MULTI_CLIENT = False
